a0ab8d4c005fa2c01dd8f4450a30f79361ba32f7
IIHasanII <hasan.abdullah0929@gmail.com>
2024-01-18 00:17:15 +0500

    added checkbox, provider

---
+++ lib/models/task_data.dart
@@ -0,0 +1,37 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:todoey/models/task.dart';
+
+class TaskData extends ChangeNotifier{
+  final List<Task> _tasks = [
+    
+  ] ;
+
+  UnmodifiableListView<Task> get tasks{
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount{
+    return _tasks.length;
+  }
+  
+  void addString (String newList){
+    
+    _tasks.add(Task(name: newList));
+    notifyListeners();
+
+  }
+
+  void updateTask (Task task){
+    task.toggleDone();
+    notifyListeners();
+
+  }
+
+  void deleteTask (Task task){
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
+
